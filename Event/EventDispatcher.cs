fbf03acaaf474e3aa311ed89fb1b1161a2cf64c8
Rick <rickjiangshu@gmail.com>
2017-11-27 17:42:28 +0800

    op closed package

--- Event/EventDispatcher.cs
+++ Event/EventDispatcher.cs
@@ -8,7 +8,9 @@ using System;
 using System.Collections.Generic;
 
 /// <summary>
-/// 事件派发器（优化装箱/拆箱）
+/// 事件派发器
+/// 1. 优化装箱/拆箱
+/// 2. 不使用闭包
 /// </summary>
 public class EventDispatcher
 {
@@ -44,6 +46,10 @@ public class EventDispatcher
     {
         AddListener(evt, (Delegate)callback);
     }
+    public void AddListener(string evt, object callback)
+    {
+    }
+
     public void AddListener(string evt, Delegate callback)
     {
         Delegate listener;
@@ -96,57 +102,96 @@ public class EventDispatcher
 
     public void Dispatch<T1, T2, T3, T4>(string evt, T1 arg1, T2 arg2, T3 arg3, T4 arg4)
     {
-        Dispatch(evt, delegate(Delegate method)
+        Delegate[] methods = GetMethods(evt);
+        if (methods != null)
         {
-            ((Action<T1, T2, T3, T4>)method)(arg1, arg2, arg3, arg4);
-        });
-    }
-    public void Dispatch<T1, T2, T3>(string evt, T1 arg1, T2 arg2, T3 arg3)
-    {
-        Dispatch(evt, delegate(Delegate method)
-        {
-            ((Action<T1, T2, T3>)method)(arg1, arg2, arg3);
-        });
-    }
-    public void Dispatch<T1, T2>(string evt, T1 arg1, T2 arg2)
-    {
-        Dispatch(evt, delegate(Delegate method)
-        {
-            ((Action<T1, T2>)method)(arg1, arg2);
-        });
-    }
-    public void Dispatch<T>(string evt, T arg)
-    {
-        Dispatch(evt, delegate(Delegate method)
-        {
-            ((Action<T>)method)(arg);
-        });
-    }
-    public void Dispatch(string evt)
-    {
-        Dispatch(evt, delegate(Delegate method)
-        {
-            ((Action)method)();
-        });
-    }
-
-    private void Dispatch(string evt, Action<Delegate> Invoke)
-    {
-        Delegate listener;
-        if (_listeners.TryGetValue(evt, out listener))
-        {
-            Delegate[] methods = listener.GetInvocationList();
             foreach (Delegate m in methods)
             {
                 try
                 {
-                    Invoke(m);
+                    ((Action<T1, T2, T3, T4>)m)(arg1, arg2, arg3, arg4);
                 }
-                catch (Exception e)
-                {
-                    UnityEngine.Debug.LogError(e);
-                }
+                catch (Exception e) { LogError(e); }
             }
         }
+    }
+
+    public void Dispatch<T1, T2, T3>(string evt, T1 arg1, T2 arg2, T3 arg3)
+    {
+        Delegate[] methods = GetMethods(evt);
+        if (methods != null)
+        {
+            foreach (Delegate m in methods)
+            {
+                try
+                {
+                    ((Action<T1, T2, T3>)m)(arg1, arg2, arg3);
+                }
+                catch (Exception e) { LogError(e); }
+            }
+        }
+    }
+
+    public void Dispatch<T1, T2>(string evt, T1 arg1, T2 arg2)
+    {
+        Delegate[] methods = GetMethods(evt);
+        if (methods != null)
+        {
+            foreach (Delegate m in methods)
+            {
+                try
+                {
+                    ((Action<T1, T2>)m)(arg1, arg2);
+                }
+                catch (Exception e) { LogError(e); }
+            }
+        }
+    }
+
+    public void Dispatch<T>(string evt, T arg)
+    {
+        Delegate[] methods = GetMethods(evt);
+        if (methods != null)
+        {
+            foreach (Delegate m in methods)
+            {
+                try
+                {
+                    ((Action<T>)m)(arg);
+                }
+                catch (Exception e) { LogError(e); }
+            }
+        }
+    }
+
+    public void Dispatch(string evt)
+    {
+        Delegate[] methods = GetMethods(evt);
+        if (methods != null)
+        {
+            foreach (Delegate m in methods)
+            {
+                try
+                {
+                    ((Action)m)();
+                }
+                catch (Exception e) { LogError(e); }
+            }
+        }
+    }
+
+    private Delegate[] GetMethods(string evt)
+    {
+        Delegate listener;
+        if (_listeners.TryGetValue(evt, out listener))
+        {
+            return listener.GetInvocationList();
+        }
+        return null;
+    }
+
+    private static void LogError(Exception e)
+    {
+        UnityEngine.Debug.LogError(e);
     }
 }
